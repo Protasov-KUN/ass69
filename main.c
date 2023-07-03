4ec731863d6569d6085e241392338e836f9db2e0
Evgeny Protasov <protaz888@gmail.com>
2023-07-03 14:00:47 +0300

    print a and array  in main

--- main.c
+++ main.c
@@ -6,5 +6,12 @@ int main()
     
     int a = 1;
    
+    puts("array: idouble b[]");
+    printf("b[0]: %.1lf\n", b[0]);
+    printf("b[1]: %.1lf\n", b[1]);
+
+    puts("array: int a");
+    printf("a: %i\n", a);
+
     return 0;
 }
