8a4b005f80dd33cb91e75dc6e25f8dd16e7b3e1e
Evgeny Protasov <protaz888@gmail.com>
2023-07-04 14:38:01 +0300

    add var c and sum a+c

--- main.c
+++ main.c
@@ -5,13 +5,19 @@ int main()
     double b[2] = {1.2, 1.2};
     
     int a = 1;
+    int c = 2;
    
     puts("array: idouble b[]");
     printf("b[0]: %.1lf\n", b[0]);
     printf("b[1]: %.1lf\n", b[1]);
 
-    puts("array: int a");
+    puts("var: int a");
     printf("a: %i\n", a);
+
+    puts("var: int c");
+    printf("c: %i\n", c);
+
+    printf("a+c: %i\n", a+c);
 
     return 0;
 }
